d0229c143b10465dc1df6980c18da1ff546c6c34
Arnau Mora <arnyminerz@proton.me>
2024-01-15 17:37:51 +0100

    Added bar color
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -1,33 +1,24 @@
 package com.ncorti.kotlin.template.app
 
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroCustomLayoutFragment
-import com.github.appintro.AppIntroFragment
 
-class MainActivity : AppIntro2() {
+class MainActivity : AppIntro() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setBarColor(0xfff)
-
-        addSlide(
-            AppIntroFragment.createInstance(
-                title = "Testing Page",
-                description = "This is the description of the first page"
-            )
+        isSkipButtonEnabled = true
+        setBarColor(
+            ResourcesCompat.getColor(resources, R.color.barColor, theme)
         )
+
         addSlide(
             AppIntroCustomLayoutFragment.newInstance(R.layout.custom_fragment_layout)
         )
-    }
-
-    override fun onSkipPressed(currentFragment: Fragment?) {
-        super.onSkipPressed(currentFragment)
-        // Decide what to do when the user clicks on "Skip"
-        finish()
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
